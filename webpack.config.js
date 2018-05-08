bea154733f45cb991f7f53c00597185c1f5177f3
david <395603360@qq.com>
2018-05-09 00:19:01 +0800

    调整编译
    添加了编译完成后打开网页

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const htmlWebpackPlugin  = require('html-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');//编译完打开链接
 const webpack = require('webpack');
-
+const config = require('./config/config');
 const webpack_conf = {
     name: 'client',
     target: 'web',
@@ -14,8 +15,17 @@ const webpack_conf = {
     },
     module: {},
 };
-
-webpack_conf.entry = ['babel-polyfill','./src/app.js'];//babel-polyfill 据说用于兼容浏览器;
+// webpack_conf.mode = 'development'; //设置环境变量，可以设置process.env.NODE_ENV的值,  但是我现在加上这句就报错了
+webpack_conf.entry = [
+    'babel-polyfill',
+    // 'webpack-hot-middleware/client',
+    // 'react-hot-loader/patch',
+    path.resolve(__dirname, 'src/app.js')
+  ];
+/*
+babel-polyfill 据说用于兼容浏览器;
+webpack-hot-middleware/client 和 react-hot-loader/patch 做热更新添加的模块
+*/
 
 webpack_conf.output = {
     path:path.resolve(__dirname, 'dist'),//输出的文件的路径，__dirname node相对运行命令的决定路径
@@ -25,11 +35,11 @@ webpack_conf.output = {
 webpack_conf.devtool = 'source-map';
 
 
-webpack_conf.devServer={
-        contentBase: path.join(__dirname, 'dist'),
-        port: 3000,
-        host: 'localhost',
-        open:true
+// webpack_conf.devServer={
+//         contentBase: path.join(__dirname, 'dist'),
+//         port: 3000,
+//         host: 'localhost',
+//         open:true
         // historyApiFallback: true,
         // inline: true,
         // // hot: true,
@@ -37,13 +47,14 @@ webpack_conf.devServer={
         //   aggregateTimeout: 300,
         //   poll: 1000
         // }
-    };
+    // };
 
 webpack_conf.plugins = [
   new htmlWebpackPlugin({
-    template:'index.html',
+    template:'./index.html',
     title:'webpack is good',
     data: new Date(),
+    // showErrors: true,// 是否将错误信息输出到html页面中
     minify:{
       removeComments:true,//去掉注释
       collapseWhitespace:true,//去掉空格
@@ -51,8 +62,10 @@ webpack_conf.plugins = [
     // chunks:['main'],//仅加载哪些js模块
   }),
   new webpack.NamedModulesPlugin(),
-  new webpack.HotModuleReplacementPlugin()//热加载插件
-  // new webpack.HotModuleReplacementPlugin()
+  new webpack.HotModuleReplacementPlugin(),//热加载插件
+  new OpenBrowserPlugin({
+          url: `http://127.0.0.1:${config.port}`
+      })
 ];
 
 
@@ -86,6 +99,10 @@ webpack_conf.plugins = [
         }, {
             test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
             use: ['url-loader?limit=10000&mimetype=image/svg+xml']
+        },
+        {
+          test: /\.(eot|woff|ttf|woff2)$/,
+          use: 'url-loader'
         }
     ];
 
